398b095392df6e68cd00ab00bf75200a03e0a6fe
nawalkanabee <nawalkanabee@gmail.com>
2022-10-25 20:44:08 +0300

    Done with arithmatic lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+let x = 31;
+let y=2;
+let multiply = x * y ;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+let num1=15;
+let num2=11;
+let mod=num1 % num2;
+console.log(mod)
+
+let max = 20;
+let min = 10;
+if (num1>num2)
+
+{
+  console.log(max)
+  
+ }
+else {
+   console.log(min)
+   }
